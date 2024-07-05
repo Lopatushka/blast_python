66f5cb96d2f54565b2c217471ed7cdd43d397494
Lopatushka <helena.lopatukhina@gmail.com>
2024-07-05 15:25:46 +0300

    fix run_blastn_alignments fun

--- scripts/filenames_parsing.py
+++ scripts/filenames_parsing.py
@@ -149,11 +149,9 @@ def run_blastn(input_file, database, num_threads=4):
                    stderr=subprocess.PIPE, check=True, shell = True, text=True)
     return p1.stdout
 
-def run_blastn_alignments(input_file, database, hits, num_threads=4):
-    command = f'blastn -query {input_file} -db {database} -num_threads {num_threads} -taxids {hits} -outfmt 0'
-    p1 = subprocess.run(command, stdout=subprocess.PIPE,
-                   stderr=subprocess.PIPE, check=True, shell = True, text=True)
-    return p1.stdout
+def run_blastn_alignments(input_file, output_file, database, hits, num_threads=4):
+    command = f'blastn -query {input_file} -db {database} -num_threads {num_threads} -taxids {hits} -outfmt 0 -out {output_file}'
+    subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True, shell = True, text=True)
 
 def blastn_results_processing(data, consensus_name=None, database=None, dir="./", qcovus_treshold=80, pident_treshold=95):
     df = pd.read_csv(io.StringIO(data), index_col=False, header = None, sep = "\t",
@@ -260,6 +258,7 @@ def main():
             result = run_blastn(pairs[0], database, num_threads=4)
             blastn_results_processing(data=result, consensus_name=pairs[0],
                     database=database, dir=dir, qcovus_treshold=80, pident_treshold=95)
+            hits = hits_taxids()
         else:
             # Merge files for alignment
             merge_name = f'{dir}/{sample_name}.fa'
