9253fa0a88d321b9175e1a7180d2830c7cbce987
Lopatushka <helena.lopatukhina@gmail.com>
2024-07-12 10:49:12 +0300

    move_files - overwrite files in the dirs

--- scripts/filenames_parsing.py
+++ scripts/filenames_parsing.py
@@ -259,8 +259,18 @@ def create_dir(new_directory):
 
 def move_files(dir, files):
     for file in files:
-        if os.path.isfile(file):
-            shutil.move(file, dir)
+        try:
+            if os.path.isfile(file):
+                destination_file = os.path.join(dir, os.path.basename(file))
+                if os.path.exists(destination_file):
+                    os.remove(destination_file)
+                    print(f"Existing file {destination_file} removed.")
+                shutil.move(file, dir)
+        except shutil.Error as e:
+            print(f"Error moving file {file} to {dir}: {e}")
+        except OSError as e:
+            print(f"OS error occurred while moving file {file}: {e}")
+        
 
 def main():
     args = parse_arguments()
