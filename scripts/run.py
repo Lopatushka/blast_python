79f1162ddda2323cf5c381299e32f909e03597a2
Lopatushka <helena.lopatukhina@gmail.com>
2024-07-15 09:48:24 +0300

    make axualiry files

--- scripts/run.py
+++ scripts/run.py
@@ -1,17 +1,3 @@
-import argparse
-import os
-import shutil
-import subprocess
-import sys
-import io
-import glob
-import warnings
-import numpy as np
-import pandas as pd
-from Bio import SeqIO
-from Bio import AlignIO
-from collections import Counter
-      
 def main():
     args = parse_arguments()
     dir = args.directory
