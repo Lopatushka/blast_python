2dd408f15dac35c17bf6e6140abe0bacbbe8fc6d
Lopatushka <helena.lopatukhina@gmail.com>
2024-07-16 11:27:54 +0300

    add rename.sh script

---
+++ scripts/rename.sh
@@ -0,0 +1,22 @@
+#!bin/bash
+: '
+Change substring in filenames.
+
+Input: path to directory with files, pattern to be changed in filenames, substitute change.
+'
+
+DIR=$1
+PATTERN=$2
+RENAME=$3
+
+FIND_RESULTS=$(find ${DIR} -maxdepth 1 -name "*${PATTERN}*")
+#echo "$FIND_RESULTS"
+
+while read FILE
+do
+
+	newfile=$(echo ${FILE} |sed "s/${PATTERN}/${RENAME}/")
+	mv "${FILE}" "${newfile}" 
+
+done <<< ${FIND_RESULTS}
+
