7f7002a314386f9d27d95359d7c18b059e821fa8
Lopatushka <helena.lopatukhina@gmail.com>
2024-07-19 22:36:00 +0300

    depcicate warning from pandas fillna

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -4,3 +4,6 @@ from .filesystem_navigation import *
 from .get_files import *
 from .make_consensus import *
 from .remove_files import *
+
+import pandas as pd
+pd.set_option('future.no_silent_downcasting', True)
