0c4256f11350f04314de6be6499a71450f5c7efb
Lopatushka <helena.lopatukhina@gmail.com>
2024-07-26 12:02:13 +0300

    filename_parsing: try-except

--- scripts/get_files.py
+++ scripts/get_files.py
@@ -48,62 +48,65 @@ def list_of_files_by_pattern(dir, extension, patterns):
 
 
 def filename_parsing(file):
-    sample_types = ["C", "CS", "F", "A", "P"]
-    #primer_names = []
-    
-    # Get filename without extension
-    filename = file.split("/")[-1][:-4]
+    try:
+        sample_types = ["C", "CS", "F", "A", "P"]
+        #primer_names = []
+        
+        # Get filename without extension
+        filename = file.split("/")[-1][:-4]
 
-    # Split filename by _ and check its length
-    filename_splitted = filename.split("_")
+        # Split filename by _ and check its length
+        filename_splitted = filename.split("_")
 
-    # Check filename splitting
-    # Length array after splitting
-    if len(filename_splitted) < 4:
-        raise ValueError(f"Error parsing filename {file}. "
-                            f"Filename has to be started with Plate-YY-MM-DD and contain the following information: "
-                            f"sample_type, sample_name, primer_name and primer orientation. "
+        # Check filename splitting
+        # Length array after splitting
+        if len(filename_splitted) < 4:
+            raise ValueError(f"Error parsing filename {file}. "
+                                f"Filename has to be started with Plate-YY-MM-DD and contain the following information: "
+                                f"sample_type, sample_name, primer_name and primer orientation. "
+                                f"For detailes see READ.me")
+
+        # Plate-YY-MM-DD
+        if (len(filename_splitted[0].split("-")) != 4) | (filename_splitted[0].split("-")[0] != "Plate"):
+            raise ValueError(f"Error parsing filename {file}: filename has to be started with 'Plate-YY-MM-DD'."
                             f"For detailes see READ.me")
-
-    # Plate-YY-MM-DD
-    if (len(filename_splitted[0].split("-")) != 4) | (filename_splitted[0].split("-")[0] != "Plate"):
-        raise ValueError(f"Error parsing filename {file}: filename has to be started with 'Plate-YY-MM-DD'."
-                         f"For detailes see READ.me")
+        
+        # Get sample type and check it
+        sample_type = filename_splitted[1]
+        if sample_type not in sample_types:
+            raise ValueError(f"Error parsing filename {file}: wrong sample_type: {sample_type}. "
+                            f"Possible options for sample_type: {sample_types}. For detailes see READ.me")
     
-    # Get sample type and check it
-    sample_type = filename_splitted[1]
-    if sample_type not in sample_types:
-        raise ValueError(f"Error parsing filename {file}: wrong sample_type: {sample_type}. "
-                         f"Possible options for sample_type: {sample_types}. For detailes see READ.me")
-  
-    # Get sample name
-    sample_name = filename_splitted[2]
+        # Get sample name
+        sample_name = filename_splitted[2]
 
-    # Get primer name
-    primer = filename_splitted[3]
-    primer_splitted = primer.split("-")
+        # Get primer name
+        primer = filename_splitted[3]
+        primer_splitted = primer.split("-")
 
-    # Check primer name
-    if len(primer_splitted) != 2:
-        raise ValueError(f"Error parsing filename {file}: wrong primer name: {primer}. "
-                         f"For detailes see READ.me")
-    elif ("F" not in primer_splitted[1]) & ("f" not in primer_splitted[1]) & ("R" not in primer_splitted[1]) & ("r" not in primer_splitted[1]):
-        raise ValueError(f"Error parsing filename {file}: wrong primer name: {primer}. "
-                         f"You need to specify primer's orientartion. "
-                         f"For detailes see READ.me")
-    #elif primer_splitted[0] not in primer_names:
-        #raise ValueError(f"Error parsing filename {file}: wrong primer name {primer}.\
-                            # For detailes see READ.me")
+        # Check primer name
+        if len(primer_splitted) != 2:
+            raise ValueError(f"Error parsing filename {file}: wrong primer name: {primer}. "
+                            f"For detailes see READ.me")
+        elif ("F" not in primer_splitted[1]) & ("f" not in primer_splitted[1]) & ("R" not in primer_splitted[1]) & ("r" not in primer_splitted[1]):
+            raise ValueError(f"Error parsing filename {file}: wrong primer name: {primer}. "
+                            f"You need to specify primer's orientartion. "
+                            f"For detailes see READ.me")
+        #elif primer_splitted[0] not in primer_names:
+            #raise ValueError(f"Error parsing filename {file}: wrong primer name {primer}.\
+                                # For detailes see READ.me")
 
-    # Get path to dir
-    dir = "/".join(file.split("/")[:-1])
-    # Check dir and parsing prosedure in general
-    if not os.path.isdir(dir):
-        raise ValueError(f"Error parsing filename {file}: directory {dir} doesn't exist")
-    
-    return {'filename': filename,
-                'sample_type': sample_type,
-                'sample_name': sample_name,
-                'primer': primer,
-                'path': file,
-                'dir': dir}
+        # Get path to dir
+        dir = "/".join(file.split("/")[:-1])
+        # Check dir and parsing prosedure in general
+        if not os.path.isdir(dir):
+            raise ValueError(f"Error parsing filename {file}: directory {dir} doesn't exist")
+        
+        return {'filename': filename,
+                    'sample_type': sample_type,
+                    'sample_name': sample_name,
+                    'primer': primer,
+                    'path': file,
+                    'dir': dir}
+    except ValueError as e:
+        print(f"Error occured: {e}")
