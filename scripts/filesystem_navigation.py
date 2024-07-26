8a1459dd81efbe070b301772aefb92c49b1be7fe
Lopatushka <helena.lopatukhina@gmail.com>
2024-07-26 12:18:11 +0300

    move_files(): fix message

--- scripts/filesystem_navigation.py
+++ scripts/filesystem_navigation.py
@@ -12,7 +12,7 @@ def move_files(dir, files):
                 destination_file = os.path.join(dir, os.path.basename(file))
                 if os.path.exists(destination_file):
                     os.remove(destination_file)
-                    print(f"Existing file {destination_file} removed.")
+                    print(f"Existing file {destination_file} will be overwritten.")
                 shutil.move(file, dir)
         except shutil.Error as e:
             print(f"Error moving file {file} to {dir}: {e}")
