b47d800aa7b32de2712c66774352cb9dee20f52b
Lopatushka <helena.lopatukhina@gmail.com>
2024-07-26 12:22:50 +0300

    blastn: change messages

--- scripts/blastn.py
+++ scripts/blastn.py
@@ -13,14 +13,14 @@ def run_blastn(input_file, database, num_threads):
                     stderr=subprocess.PIPE, check=True, shell = True, text=True)
         return p1.stdout
     except subprocess.CalledProcessError as e:
-        print(f"Error occured during blastn search: {e}")
+        print(f"There is no match for query {input_file} in blastn database {database}. Error message is: {e}")
 
 def run_blastn_alignments(input_file, output_file, database, hits, num_threads):
     try:
         command = f'blastn -query {input_file} -db {database} -num_threads {num_threads} -taxids {hits} -outfmt 0 -out {output_file}'
         subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True, shell = True, text=True)
     except subprocess.CalledProcessError as e:
-        print(f"Error occured during blastn alignments run: {e}")
+        print(f"Error occured during blastn alignments run for query {input_file} and blastn database {database}. Error message is: {e}")
 
 def blastn_results_processing(data, qcovus_treshold, pident_treshold, consensus_name=None, database=None, dir="./"):
     try:
