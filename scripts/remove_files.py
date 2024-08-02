68e28b17edbf97b4530ae2b870f22d19129fac03
Lopatushka <helena.lopatukhina@gmail.com>
2024-08-02 15:09:31 +0300

    add fun remove_files_by_pattern

--- scripts/remove_files.py
+++ scripts/remove_files.py
@@ -1,8 +1,20 @@
 import os
 import subprocess
 import glob
+import warnings
+from .get_files import list_of_files
 
-def remove_file_by_pattern(dir, pattern):
+def remove_files_by_pattern(directory, pattern):
+    # List all files in the directory
+    fasta_files = list_of_files(directory, "fa")
+    matching_files = [file for file in fasta_files if pattern in file]
+    if matching_files:
+        for file in matching_files:
+            subprocess.run(['rm', file], check=True)
+    else:
+         warnings.warn(f"There is no files in the directory {directory} with pattern {pattern} to delete.")
+
+def remove_file_by_pattern_old(dir, pattern):
     path = os.path.join(dir, pattern)
     matching_files = glob.glob(path)
     if matching_files:
