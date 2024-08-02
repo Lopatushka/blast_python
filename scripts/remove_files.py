45d4d51e7b48fe4a4693558d4affe53896ac51c2
Lopatushka <helena.lopatukhina@gmail.com>
2024-08-02 15:19:25 +0300

    add new fun remove_files_with_extension. change remove_files_by_pattern fun

--- scripts/remove_files.py
+++ scripts/remove_files.py
@@ -4,24 +4,15 @@ import glob
 import warnings
 from .get_files import list_of_files
 
-def remove_files_by_pattern(directory, pattern):
+def remove_files_by_pattern(directory, extention, pattern):
     # List all files in the directory
-    fasta_files = list_of_files(directory, "fa")
+    fasta_files = list_of_files(directory, extention)
     matching_files = [file for file in fasta_files if pattern in file]
     if matching_files:
         for file in matching_files:
             subprocess.run(['rm', file], check=True)
     else:
          warnings.warn(f"There is no files in the directory {directory} with pattern {pattern} to delete.")
-
-def remove_file_by_pattern_old(dir, pattern):
-    path = os.path.join(dir, pattern)
-    matching_files = glob.glob(path)
-    if matching_files:
-        for file in matching_files:
-            subprocess.run(['rm', file], check=True)
-    else:
-        pass
 
 def remove_file(file_path):
     if os.path.exists(file_path):
@@ -32,8 +23,11 @@ def remove_file(file_path):
     else:
         pass
 
-# dir = "../sanger_seq/240822"
-# pattern = ".fq"
-# path = os.path.join(dir, pattern)
-# matching_files = glob.glob(path)
-# print(matching_files)
+def remove_files_with_extension(dir, extension):
+    pattern  = os.path.join(dir, f'*.{extension}')
+    matching_files = glob.glob(pattern)
+    if matching_files:
+        for file_path  in matching_files:
+            subprocess.run(['rm', file_path], check=True)
+    else:
+        print(f"There is no files with the extension .{extension} in the directory {dir}")
