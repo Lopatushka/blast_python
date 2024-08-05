c5333f4b5eb795120fad0e59906d947ca9e55290
Lopatushka <helena.lopatukhina@gmail.com>
2024-08-05 09:44:57 +0300

    make consensus: add errors catching

--- scripts/make_consensus.py
+++ scripts/make_consensus.py
@@ -34,17 +34,22 @@ def reverse_complement_fasta(input_fasta, output_fasta):
         print(f"Error occured with file processing {input_fasta}: {e}")
 
 def merge_fasta_files(input_fasta_files, output_fasta):
-    with open(output_fasta, "w") as output_handle:
-        for input_file in input_fasta_files:
-            records = SeqIO.parse(input_file, "fasta")
-            SeqIO.write(records, output_handle, "fasta")
+    try:
+        with open(output_fasta, "w", encoding='utf-8') as output_handle:
+            for input_file in input_fasta_files:
+                records = SeqIO.parse(input_file, "fasta")
+                SeqIO.write(records, output_handle, "fasta")
+    except UnicodeEncodeError as e:
+        print(f"Unicode encoding error during merging files: {input_fasta_files}. Error message: {e}")
+    except Exception as e:
+        print(f"An error occurred during merging files: {input_fasta_files}. Error message: {e}")
 
 def run_clustalw(input_fasta):
     try:
         command = f'clustalw2 -INFILE={input_fasta}'
         subprocess.run(command, capture_output=True, check=True, shell = True)
     except subprocess.CalledProcessError as e:
-        print(f"Error occured during clustalw run: {e}")
+        print(f"Error occured during clustalw run for file {input_fasta}. Error message: {e}")
 
 def get_custom_consensus_from_aln(aln_file, consensus_fa, threshold=0.6):
     try:
@@ -92,13 +97,21 @@ def get_custom_consensus_from_aln(aln_file, consensus_fa, threshold=0.6):
         print(f"Error occured with file processing {aln_file}: {e}")
     except FileNotFoundError as e:
         print(f"Error occured with file processing {consensus_fa}: {e}")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
 
 def check_consensus_quality(fasta, threshold):
-    consensus = SeqIO.read(fasta, "fasta")
-    consensus_length = len(consensus.seq)
-    N_count = consensus.count("N")
-    N_precentage = np.round(100*N_count/consensus_length, 3)
-    if N_precentage >= threshold:
-       return False # bad consensus
-    else:
-        return True # good consensus
+    try:
+        consensus = SeqIO.read(fasta, "fasta")
+        consensus_length = len(consensus.seq)
+        N_count = consensus.count("N")
+        N_precentage = np.round(100*N_count/consensus_length, 3)
+        if N_precentage >= threshold:
+            return False # bad consensus
+        else:
+            return True # good consensus
+    except FileNotFoundError as e:
+        print(f"Error occured with file processing {fasta}: {e}")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+
