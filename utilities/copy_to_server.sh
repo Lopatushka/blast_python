8ce23055d307d7c319b2e3027dda83f8b3cd7510
Lopatushka <helena.lopatukhina@gmail.com>
2024-08-08 14:23:13 +0300

    copy_to_server.sh: change path

--- utilities/copy_to_server.sh
+++ utilities/copy_to_server.sh
@@ -3,4 +3,4 @@
 FROM_DIR=$1
 
 TARGET_DIR_NAME=$(basename "$FROM_DIR")
-scp -r $FROM_DIR elopatuhina@10.100.20.211:/home/elopatuhina/sanger_seq/${TARGET_DIR_NAME}
+scp -r $FROM_DIR elopatuhina@10.100.20.211:/home/elopatuhina/sanger/${TARGET_DIR_NAME}
