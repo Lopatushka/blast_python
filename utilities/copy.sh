20cad078eaeee717fc705293901d5b41f9a4e030
Lopatushka <helena.lopatukhina@gmail.com>
2024-08-08 10:07:04 +0300

    copy.sh: change path

--- utilities/copy.sh
+++ utilities/copy.sh
@@ -2,7 +2,8 @@
 
 DIR_NAME=$1
 
-FROM_DIR=/mnt/c/users/ELopatuhina/Desktop/Work/Data/Raw_data/seq/${DIR_NAME}
+#FROM_DIR=/mnt/c/users/ELopatuhina/Desktop/Work/Data/Raw_data/seq/${DIR_NAME}
+FROM_DIR=/mnt/c/users/ELopatuhina/Downloads/${DIR_NAME}
 TARGET_DIR=/home/lopatushka/sanger_seq
 
 mkdir -p "${TARGET_DIR}"
