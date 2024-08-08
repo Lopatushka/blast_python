2568fd7964b517470fd3b5c98f0f56c763ed03fc
Lopatushka <helena.lopatukhina@gmail.com>
2024-08-08 12:19:18 +0300

    fix copy_from_google.sh

--- utilities/copy_from_google.sh
+++ utilities/copy_from_google.sh
@@ -4,7 +4,7 @@ FOLDER_ID=$1
 FOLDER_TO_SAVE=$2
 
 # Create the destination directory if it doesn't exist
-mkdir -p $FOLDER_TO_COPY
+mkdir -p $FOLDER_TO_SAVE
 
 # Download the folder using gdown
 gdown -O $FOLDER_TO_SAVE --folder https://drive.google.com/drive/folders/$FOLDER_ID
