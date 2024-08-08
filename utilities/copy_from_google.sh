9929c084d7488b7a39ed675a427a43e957b86499
Lopatushka <helena.lopatukhina@gmail.com>
2024-08-08 12:24:32 +0300

    change .sh file

--- utilities/copy_from_google.sh
+++ utilities/copy_from_google.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-FOLDER_ID=$1
-FOLDER_TO_SAVE=$2
+FOLDER_TO_SAVE=$1
+LINK=$2
 
 # Create the destination directory if it doesn't exist
 mkdir -p $FOLDER_TO_SAVE
 
 # Download the folder using gdown
-gdown -O $FOLDER_TO_SAVE --folder https://drive.google.com/drive/folders/$FOLDER_ID
+gdown -O $FOLDER_TO_SAVE --folder $LINK
 
 echo "Files sucessfully donwloaded and moved to folder $FOLDER_TO_SAVE"
