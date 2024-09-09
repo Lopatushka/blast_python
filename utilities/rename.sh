3226f45114c1988255887db7c809a37fdec3159d
Elena Lopatuhina <helena.lopatukhina@gmail.com>
2024-09-09 14:20:02 +0300

    rename.sh: add annotation

--- utilities/rename.sh
+++ utilities/rename.sh
@@ -1,8 +1,19 @@
 #!bin/bash
 : '
-Change substring in filenames.
+Description:
+    This script is designed to rename files in a specified directory by changing a specific pattern in their filenames.
+	The user provides a directory path, a pattern to search for in filenames, and the replacement string.
+	The script scans the files in the given directory and modifies their filenames by substituting the specified pattern with the new string.
 
-Input: path to directory with files, pattern to be changed in filenames, substitute change.
+Usage:
+    ./rename_files.sh <directory> <pattern> <substitute>
+
+    <directory>   : Path to the directory containing files to be renamed.
+    <pattern>     : Substring or pattern in the filenames that needs to be replaced.
+    <substitute>  : The string that will replace the pattern in the filenames
+
+Example:
+    ./rename_files.sh /home/user/documents old-pattern new-pattern
 '
 
 DIR=$1
