298659e753ea6154a395b6901733aff43876bab4
Elena Lopatuhina <helena.lopatukhina@gmail.com>
2024-09-09 14:19:32 +0300

    copy.sh: add annotation

--- utilities/copy.sh
+++ utilities/copy.sh
@@ -1,10 +1,17 @@
 #!bin/bash
+: '
+Description:
+    This script is designed to copy a directory from a source path (within the Windows Subsystem for Linux (WSL) environment)
+    to a target directory on the local Linux filesystem.
+    The directory to be copied is specified by the user as a command-line argument,
+    and the script creates the target directory if it does not exist before performing the copy operation.
+'
 
 DIR_NAME=$1
+#TARGET_DIR=$2
 
-#FROM_DIR=/mnt/c/users/ELopatuhina/Desktop/Work/Data/Raw_data/seq/${DIR_NAME}
 FROM_DIR=/mnt/c/users/ELopatuhina/Downloads/${DIR_NAME}
-TARGET_DIR=/home/lopatushka/sanger
+TARGET_DIR=/home/elopatuhina/sanger/${DIR_NAME}
 
 mkdir -p "${TARGET_DIR}"
 
