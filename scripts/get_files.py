05a4cc8265b57b3ad71de17700f51008fd6a5e4f
Lopatushka <helena.lopatukhina@gmail.com>
2024-09-12 10:47:47 +0300

    get_files: check arguments

--- scripts/get_files.py
+++ scripts/get_files.py
@@ -2,20 +2,72 @@ import os
 import argparse
 import warnings
 
+def is_valid_directory(path):
+    """
+    Check if the directory exists."""
+    if not os.path.isdir(path):
+        raise argparse.ArgumentTypeError(f"Directory '{path}' does not exist.")
+    return path
+
+def check_range(min_value, max_value):
+    """Return a function that checks if an integer is within the desired range."""
+    def validate(value):
+        try:
+            value = int(value)
+        except ValueError as e:
+            raise argparse.ArgumentTypeError(e)
+        if value < min_value or value > max_value:
+            raise argparse.ArgumentTypeError(f"Value '{value}' is outside the range [{min_value}, {max_value}].")
+        return value
+    return validate
+
+def check_blast_database(database):
+    try:
+        # Check if dir exists
+        dirname = os.path.dirname(database)
+        if not os.path.isdir(dirname):
+            raise argparse.ArgumentTypeError(f"Path '{database}' to blastn database is not found")
+        
+        # Check taxonomy_files are located in blast database dir
+        list_of_files = os.listdir(dirname)
+        taxonomy_files = ['taxdb.btd', 'taxdb.bti', 'taxonomy4blast.sqlite3']
+        for taxonomy_file in taxonomy_files:
+            if taxonomy_file not in list_of_files:
+                raise argparse.ArgumentTypeError(f"{taxonomy_file} is not found in the blastn database directory '{dirname}'")
+            
+        # Check validity of database name     
+        database_name = database.split("/")[-1]
+        for taxonomy_file in taxonomy_files:
+            list_of_files.remove(taxonomy_file)
+        if sum([file[:-4] == database_name for file in list_of_files]) == 0:
+            raise argparse.ArgumentTypeError(f"Wrong blastn database name: '{database_name}'")
+        return database
+    except argparse.ArgumentTypeError:
+        raise # Re-raise exceptions above 
+    except Exception as e:
+        raise argparse.ArgumentTypeError(f"Unexpected error occured during --database argument checking: {e}")
+
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Reads trimming, build consensus and perform blastn.")
-    parser.add_argument('--directory', '-d', type=str, help='Directory containing .ab1 files')
+    parser.add_argument('--directory', '-d', type=is_valid_directory, required=True, help='Directory containing .ab1 files')
     parser.add_argument('--parsing_mode', '-pm', type=str, default="auto", choices=["auto", "manual"], help='Filenames parsing mode. Options: auto (default), manual')
     parser.add_argument('--parsing_patterns', '-pp', nargs='*', type=str, default=list(), help='Unique patterns in filenames of .ab1 files for manual consensus building')
     parser.add_argument('--blastn_mode', '-bm', type=str, default="auto", choices=["auto", "manual"], help='Balstn search mode. Options: auto (default), manual')
     parser.add_argument('--consensus_patterns', '-cp', nargs='*', type=str, default=list(), help='Unique patterns in filenames of consensuses for manual blastn search')
-    parser.add_argument('--consensus_quality', '-cq', type=int, default=10, help="Maximum percetage of 'N' or degenerate nucleotides in consensus. Default value = 10")
-    parser.add_argument('--nthreads', '-nt', type=int, default=4, help='Number of threads for blastn search. Default value = 4')
-    parser.add_argument('--trimming_quality', '-tq', type=int, default=15, help='Trimming quality. Default value = 15')
-    parser.add_argument('--minlength', '-ml', type=int, default=50, help='Minimum length of consensus. Default value = 50')
-    parser.add_argument('--database', '-db', type=str, help='Blastn database')
-    parser.add_argument('--qcovus', '-qc', type=int, default=80, help='Query coverage threshold. Default value = 80')
-    parser.add_argument('--pident', '-pi', type=int, default=95, help='Percent of identity threshold. Default value = 95')
+    
+    parser.add_argument('--consensus_quality', '-cq', type=check_range(1, 100), default=10,
+                        help="Maximum percetage of 'N' or degenerate nucleotides in consensus. Permissiable value range is [1, 100]. Default value is 10.")
+    parser.add_argument('--nthreads', '-nt', type=check_range(1, 100), default=4,
+                        help='Number of threads for blastn search. Permissiable value range is [1, 100]. Default value is 4.')
+    parser.add_argument('--trimming_quality', '-tq', type=check_range(1, 100), default=15,
+                        help='Trimming quality. Permissiable value range is [1, 100]. Default value is 15.')
+    parser.add_argument('--minlength', '-ml', type=check_range(50, 1000), default=50,
+                        help='Minimum length of consensus. Permissiable value range is [1, 1000]. Default value is 50.')
+    parser.add_argument('--database', '-db', type=check_blast_database, required=True, help='Blastn database')
+    parser.add_argument('--qcovus', '-qc', type=check_range(1, 100), default=80,
+                        help='Query coverage threshold. Permissiable value range is [1, 100]. Default value is 80.')
+    parser.add_argument('--pident', '-pi', type=check_range(1, 100), default=95,
+                        help='Percent of identity threshold. Permissiable value range is [1, 100]. Default value is 95.')
     return parser.parse_args()
 
 def list_of_files(dir, extension):
